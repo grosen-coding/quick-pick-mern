95189641217e830ce39e18abd9b62da249bb59e8
grosen-coding <rosengsr@gmail.com>
2023-01-10 17:22:42 -0500

    added TMBD endpoints

--- server/src/tmdb/tmdb.endpoints.js
+++ server/src/tmdb/tmdb.endpoints.js
@@ -11,4 +11,15 @@ const tmdbEndpoints = {
     tmdbConfig.getUrl(`${mediaType}/${mediaId}/credits`),
   mediaVideos: ({ mediaType, mediaId }) =>
     tmdbConfig.getUrl(`${mediaType}/${mediaId}/videos`),
+  mediaRecommend: ({ mediaType, mediaId }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaId}/recommendations`),
+  mediaImages: ({ mediaType, mediaId }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaId}/images`),
+  mediaSearch: ({ mediaType, query, page }) =>
+    tmdbConfig.getUrl(`search/${mediaType}`, { query, page }),
+  personDetail: ({ personId }) => tmdbConfig.getUrl(`person/${personId}`),
+  personMedias: ({ personId }) =>
+    tmdbConfig.getUrl(`person/${personId}/combined_credits`),
 };
+
+export default tmdbEndpoints;

e011f9ed282f9b9b0182f9f36407178e393a7482
grosen-coding <rosengsr@gmail.com>
2023-01-11 17:01:42 -0500

    finished modules

--- client/src/api/modules/media.api.js
+++ client/src/api/modules/media.api.js
@@ -1,5 +1,59 @@
-import PrivateClient from "../client/private.client.js"
-import PublicClient from "../client/public.client.js"
+import privateClient from "../client/private.client.js";
+import publicClient from "../client/public.client.js";
 
+const mediaEndPoints = {
+  list: ({ mediaType, mediaCategory, page }) =>
+    `${mediaType}/${mediaCategory}?page=${page}`,
+  detail: ({ mediaType, mediaId }) => `${mediaType}/detail/${mediaId}`,
+  search: ({ mediaType, query, page }) =>
+    `${mediaType}/search?query=${query}&page=${page}`,
+};
 
-const mediaEndPoints
+const mediaAPI = {
+  getList: async ({ mediaType, mediaCategory, page }) => {
+    try {
+      const response = await publicClient.get(
+        mediaEndPoints.list({
+          mediaType,
+          mediaCategory,
+          page,
+        })
+      );
+
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+  getDetail: async ({ mediaType, mediaId }) => {
+    try {
+      const response = await privateClient.get(
+        mediaEndPoints.detail({
+          mediaType,
+          mediaId,
+        })
+      );
+
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+  search: async ({ mediaType, query, page }) => {
+    try {
+      const response = await publicClient.get(
+        mediaEndPoints.search({
+          mediaType,
+          query,
+          page,
+        })
+      );
+
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+};
+
+export default mediaAPI;

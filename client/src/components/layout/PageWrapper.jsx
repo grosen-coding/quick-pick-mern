fd5c8b711bd1c928bc53c218d39f3ecb3852d38f
grosen-coding <rosengsr@gmail.com>
2023-01-11 19:14:12 -0500

    added empty shells for pages and layout

---
+++ client/src/components/layout/PageWrapper.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setAppState } from "../../redux/features/appStateSlice";
+
+const PageWrapper = ({ state, children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setAppState(state));
+  }, [state]);
+
+  return children;
+};
+
+export default PageWrapper;

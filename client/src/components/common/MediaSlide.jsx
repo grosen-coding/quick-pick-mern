feaa8359bb5fb7143d0de9e52bc5794688a2033d
grosen-coding <rosengsr@gmail.com>
2023-01-14 21:18:59 -0500

    adding blank sections to display different tmdb categories

---
+++ client/src/components/common/MediaSlide.jsx
@@ -0,0 +1,9 @@
+const MediaSlide = ({ mediaType, mediaCategory }) => {
+  return (
+    <>
+      <h1>Media Slide</h1>
+    </>
+  );
+};
+
+export default MediaSlide;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import HeroSlide from "../components/common/HeroSlide";
 import tmdbConfigs from "../api/configs/tmdb.config";
+import { Box } from "@mui/system";
+import uiConfigs from "../configs/ui.configs";
+import Container from "../components/common/Container";
+import MediaSlide from "../components/common/MediaSlide";
 
 const HomePage = () => {
   return (
@@ -9,6 +13,33 @@ const HomePage = () => {
         mediaType={tmdbConfigs.mediaType.movie}
         mediaCategory={tmdbConfigs.mediaCategory.popular}
       />
+
+      <Box marginTop="-4rem" sx={{ ...uiConfigs.style.mainContent }}>
+        <Container header="popular movies">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.movie}
+            mediaCategory={tmdbConfigs.mediaCategory.popular}
+          />
+        </Container>
+        <Container header="popular series">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.tv}
+            mediaCategory={tmdbConfigs.mediaCategory.popular}
+          />
+        </Container>
+        <Container header="top rated movies">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.movie}
+            mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+          />
+        </Container>
+        <Container header="top rated tv series">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.tv}
+            mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+          />
+        </Container>
+      </Box>
     </>
   );
 };

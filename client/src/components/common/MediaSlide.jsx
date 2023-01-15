08e744e275e92d2cd1c38036b46fc1d9ac572c71
grosen-coding <rosengsr@gmail.com>
2023-01-14 21:54:00 -0500

    added MediaDetail and Swipers to each Media Section

--- client/src/components/common/MediaSlide.jsx
+++ client/src/components/common/MediaSlide.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import { SwiperSlide } from "swiper/react";
+import mediaAPI from "../../api/modules/media.api";
+import AutoSwiper from "./AutoSwiper";
+import { toast } from "react-toastify";
+import MediaItem from "./MediaItem";
+
 const MediaSlide = ({ mediaType, mediaCategory }) => {
+  const [medias, setMedias] = useState([]);
+
+  useEffect(() => {
+    const getMedia = async () => {
+      const { response, err } = await mediaAPI.getList({
+        mediaType,
+        mediaCategory,
+        page: 1,
+      });
+
+      if (response) setMedias(response.results);
+      if (err) toast.error(err.message);
+    };
+
+    getMedia();
+  }, [mediaType, mediaCategory]);
+
   return (
-    <>
-      <h1>Media Slide</h1>
-    </>
+    <AutoSwiper>
+      {medias.map((media, index) => (
+        <SwiperSlide key={index}>
+          <MediaItem media={media} mediaType={mediaType} />
+        </SwiperSlide>
+      ))}
+    </AutoSwiper>
   );
 };
 

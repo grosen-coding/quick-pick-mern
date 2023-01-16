59a5799a971a5b91bfedbc540f9bb72a282980c2
grosen-coding <rosengsr@gmail.com>
2023-01-16 16:14:22 -0500

    changed the way fonts were imported

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,11 +12,6 @@ import "swiper/css/scrollbar";
 
 import "../src/styles/style.css";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>

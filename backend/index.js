2af90f8ce458600c8869456b884e6fff2da87059
grosen-coding <rosengsr@gmail.com>
2023-01-15 20:38:27 -0500

    trying to still deploy

--- backend/index.js
+++ backend/index.js
@@ -39,16 +39,16 @@ app.use("/api/v1", routes);
 const server = http.createServer(app);
 
 // Serve frontend
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
-//     )
-//   );
-// } else {
-//   app.get("/", (req, res) => res.send("Please set to production"));
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 const port = process.env.PORT || 3000;

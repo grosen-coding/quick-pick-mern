ea20d7f583f70238e8d993f95297d0f6fe47cf66
grosen-coding <rosengsr@gmail.com>
2023-01-17 01:02:41 -0500

    removed cors and cookieparser

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import http from "http";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import http from "http";
 import colors from "colors";
 import path from "path";
 import mongoose from "mongoose";
@@ -39,10 +39,10 @@ const app = express();
 
 // console.log(process.env);
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.use("/api/v1", routes);
 

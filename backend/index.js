909616e637ee5ba663923ff2e3928d30fff0150d
grosen-coding <rosengsr@gmail.com>
2023-01-16 21:31:51 -0500

    fixed bugs in index.js backend file to enable heroku deploy

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,19 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
 import colors from "colors";
+import path from "path";
 import mongoose from "mongoose";
 import "dotenv/config";
 import routes from "./src/routes/index.js";
+import url from "url";
+
 const port = process.env.PORT || 3000;
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename);
+console.log(__dirname);
 
 mongoose.set("strictQuery", false);
 
@@ -17,9 +26,9 @@ mongoose
   })
   .then(() => {
     console.log(`Mongo DB Connected!`.cyan.underline);
-    server.listen(port, () => {
-      console.log(`Server is listening on port ${port}`.green.italic);
-    });
+    // server.listen(port, () => {
+    //   console.log(`Server is listening on port ${port}`.green.italic);
+    // });
   })
   .catch((err) => {
     console.log({ err });
@@ -38,17 +47,19 @@ app.use(cookieParser());
 app.use("/api/v1", routes);
 
 // Server
-const server = http.createServer(app);
-
+// const server = http.createServer(app);
+console.log("Dir name-=", __dirname);
 // Serve frontend
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
-//     )
-//   );
-// } else {
-//   app.get("/", (req, res) => res.send("Please set to production"));
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
